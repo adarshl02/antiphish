26e80960b0322f1c032a8d41905b23d638326cf8
adarshl02 <adarsh.landge10604@gmail.com>
2025-09-30 21:04:36 +0530

    change google client id

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AuthProvider } from "./providers/AuthProvider.tsx";
 
-const GOOGLE_CLIENT_ID = "459809883895-foa9fm61p0659q0u32d070npnrrupbjs.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "150246280801-foqhoto1gum0uef7j0o2ej2q3v281i2p.apps.googleusercontent.com";
 
 createRoot(document.getElementById("root")!).render(
     <AuthProvider>
